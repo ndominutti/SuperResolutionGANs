e3e360a5f5913e2c565bb6a091abd54f8d320fa8
ndominutti <ndominutti@gmail.com>
2023-11-05 22:37:59 +0000

    Add discriminator loss

--- SRGAN/train.py
+++ SRGAN/train.py
@@ -5,23 +5,40 @@ def train_step(dataloader,
                generator,
                discriminator,
                vggloss,
-               lr
+               lr,
+               bce,
+               optimizer_disc,
+               optimizer_gen
               ):
-
-    optimizer = optim.Adam(generator.parameters(), lr=lr)
     
     for high_res, low_res in tqdm(dataloader):
         high_res.to('cpu')
         low_res.to('cpu')
+        gen_img   = generator(low_res)
+        
+        ### DISCRIMINATOR LOSS
+        disc_gen  = discriminator(gen_img.detach())
+        disc_real = discriminator(high_res)
+        #Use BCEWithLogitLoss to train. 
+        #For real cases the calculus is between the output from de Discriminator for
+        #the real images and a tensor full of 1s (because real images label is 1)
+        #Then add some noise just to improve training stability and prevent the discriminator
+        #from becoming too confident too early in training
+        disc_loss_real = bce(
+            disc_real, torch.ones_like(disc_real) - 0.1 * torch.rand_like(disc_real)
+        )
+        disc_loss_gen = bce(disc_gen, torch.zeros_like(disc_gen))
+        loss_disc = disc_loss_fake + disc_loss_real
 
-        #Use Mixed Precision Training
-        with torch.cpu.amp.autocast():
-            gen_img   = generator(low_res)
-            disc_gen_img  = discriminator(gen_img.detach())
-            disc_real_img = discriminator(high_res)
-            disc_loss = (
-                -(torch.mean(disc_real_img) - torch.mean(disc_gen_img))
-            )
+        optimizer_disc.zero_grad()
+        loss_disc.backward()
+        optimizer_disc.step()
+
+
+        ### GENERATOR LOSS
+        
+
+
 
         
         
