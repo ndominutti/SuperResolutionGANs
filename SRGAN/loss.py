c2b7a3157e8e5dcd27f6642649331c1cf3299e1b
ndominutti <ndominutti@gmail.com>
2023-11-05 20:29:07 +0000

    Add loss function

---
+++ SRGAN/loss.py
@@ -0,0 +1,17 @@
+from torchvision.models import vgg19
+import torch.nn as nn
+
+#phi 5,4 
+class VGGLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vgg = vgg19(pretrained=True).features[:36].eval().to('cpu') #this will get by default up to the last Convolution beforr the MaxPooling layer, the fifth one, that happens to be the one used to compute the vgg loss for the best performing model in the paper
+        for param in self.vgg.parameters():
+            param.requires_grad=False
+        self.mse = nn.MSELoss()
+
+    def forward(gen, gt):
+        gen_vgg = self.vgg(gen)
+        gt_vgg  = self.vgg(gt)
+        return self.mse(gen_vgg, gt_vgg)
+        
