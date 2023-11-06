4441f8e4b33907c2a75533fcb25dd779068199ff
ndominutti <ndominutti@gmail.com>
2023-11-06 23:45:11 +0000

    Add logger. Finish training step. Add main training functionality

--- SRGAN/train.py
+++ SRGAN/train.py
@@ -1,11 +1,25 @@
 from tqdm import tqdm
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from models import Generator, Discriminator
+from loss import VGGLoss
+import torch.nn as nn
+import argparse
+import logging
+import sys
+import dataset_handler
+
+
+# Set up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
 
 def train_step(dataloader,
                generator,
                discriminator,
                vggloss,
-               lr,
                bce,
                optimizer_disc,
                optimizer_gen
@@ -35,7 +49,63 @@ def train_step(dataloader,
         optimizer_disc.step()
 
 
-        ### GENERATOR LOSS
+        ### GENERATOR LOSS (Perceptual loss)
+        content_loss     = 0.006 * vggloss(gen_img, high_res) #0.006 equals to the 1/12.75 rescaling factor used in the paper
+        disc_gen  = discriminator(gen_img)
+        adversarial_loss = -1e-2*bce(disc_gen, torch.ones_like(disc_fake))
+        perceptual_loss  = content_loss + adversarial_loss
+
+        optimizer_gen.zero_grad()
+        perceptual_loss.backward()
+        optimizer_gen.step()
+
+
+def train(args):
+    """
+    Main training pipeline
+    """
+    logger.info("Loading dataset...\n")
+    train_dataset = dataset_handler.load_train_dataset(
+        args.train_data_dir
+    )        
+    logger.info("Creating dataloader...\n")
+    dataloader     = DataLoader(
+        train_dataset,
+        batch_size=args.train_batch_size,
+        shuffle=True,
+    )
+    generator      = Generator()
+    discriminator  = Discriminator()
+    vggloss        = VGGLoss()
+    bce            = nn.BCEWithLogitsLoss()
+    mse            = nn.MSELoss()
+    optimizer_disc = optim.Adam(discriminator.parameters(), lr=args.lr, betas=(0.9, 0.999))
+    optimizer_gen  = optim.Adam(generator.parameters(), lr=args.lr, betas=(0.9, 0.999))
+
+    logger.info("Training started...\n")
+    for epoch in range(args.epochs):
+        train_step(dataloader,
+                   generator,
+                   discriminator,
+                   vggloss,
+                   bce,
+                   optimizer_disc,
+                   optimizer_gen
+                  )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int)
+    parser.add_argument("--train-data-dir", type=str, default='data/train/')
+    parser.add_argument("--train-batch-size", type=int, default=4)
+    parser.add_argument("--lr", type=float, default=10e-3)
+    train(parser.parse_args())
+    
+    
+
+    
+        
         
 
 
