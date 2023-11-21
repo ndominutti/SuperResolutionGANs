f7d29a77b2bd0b78fd9929305dc4e64b799a1a26
ndominutti <ndominutti@gmail.com>
2023-11-21 00:04:06 +0000

    Improve repo structure

---
+++ RealESRGAN/train/scripts/train.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+import os.path as osp
+from basicsr.train import train_pipeline
+
+# import realesrgan.archs
+# import realesrgan.data
+# import realesrgan.models
+
+if __name__ == '__main__':
+    root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
+    print('Root path: '+root_path)
+    train_pipeline(root_path)
