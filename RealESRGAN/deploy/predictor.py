21812c11d307a4a97ab806f99b52e25c7712bc20
ndominutti <ndominutti@gmail.com>
2023-11-21 00:42:54 +0000

    Docstring generation

--- RealESRGAN/deploy/predictor.py
+++ RealESRGAN/deploy/predictor.py
@@ -2,8 +2,8 @@ from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi import FastAPI, Response, Request
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
-import utils
 import numpy as np
+import utils
 import json
 import cv2
 import io
@@ -16,12 +16,28 @@ KWARGS      = config_dict['model_params']
 
 class UpscalingService(object):
     """
+    Create a upscaling service with a RealESRGANer model (https://github.com/xinntao/Real-ESRGAN/blob/master/realesrgan/utils.py) under the hood.
     """
     model = None
 
     @classmethod
-    def get_model(upscaler_service, model_path, tile, tile_pad, pre_pad, fp32, device):
-        """Get the model object for this instance, loading it if it's not already loaded."""
+    def get_model(upscaler_service, model_path:str, tile:int, tile_pad:int, pre_pad:int, fp32:bool, device:str) -> None:
+        """
+        Get the model object for this instance, loading it if it's not already loaded.
+
+        Args:
+            model_path(str): Path to the model's weights
+            tile (int)     : As too large images result in the out of GPU memory issue, so this tile option will first crop
+            input images into tiles, and then process each of them. Finally, they will be merged into one image.
+            0 denotes for do not use tile. Default: 0.
+            tile_pad (int) : The pad size for each tile, to remove border artifacts. Default: 10.
+            pre_pad (int)  : Pad the input images to avoid border artifacts. Default: 10.
+            fp32(bool)     : If set to false, use half precision during inference (only available for cuda device).
+            device(str)    : Define the device to use
+
+        Returns 
+            None
+        """
         if upscaler_service.model == None:
             model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
             upscaler_service.model = RealESRGANer(
@@ -38,23 +54,38 @@ class UpscalingService(object):
             )
             
     @classmethod
-    def preprocess(upscaler_service, np_image):
+    def preprocess(upscaler_service, np_image:np.ndarray) -> np.ndarray:
         """
+        Apply minor preprocessing to the input image.
+
+        Args:
+            np_image(np.array): image in form of a numpy array
+
+        Returns
+            np.ndarray: decoded image
         """
         im2 = cv2.imdecode(np_image, cv2.IMREAD_UNCHANGED)
         return im2    
         
     @classmethod
-    def predict(upscaler_service, np_image, tile, tile_pad, pre_pad, fp32,
-                 outscale, device):
-        """For the input, do the predictions and return them.
+    def predict(upscaler_service, np_image:np.ndarray, tile:0, tile_pad:0, pre_pad:0, fp32:bool,
+                 outscale:int, device:str) -> np.ndarray:
+        """For the input, preprocess them and then do the predictions and return them.
 
         Args:
-            input (pd.DataFrame): The data on which to do the predictions. There will be
-                one prediction per row in the dataframe
+            np_image(np.ndarray): Input image in the form of a numpy ndarray
+            tile (int)          : As too large images result in the out of GPU memory issue, so this tile option will first crop
+            input images into tiles, and then process each of them. Finally, they will be merged into one image.
+            0 denotes for do not use tile. Default: 0.
+            tile_pad (int)      : The pad size for each tile, to remove border artifacts. Default: 10.
+            pre_pad (int)       : Pad the input images to avoid border artifacts. Default: 10.
+            fp32(bool)          : If set to false, use half precision during inference (only available for cuda device).
+            outscale(int)       : rezising scale to be used (HAVE IN MIND HIS IMPLEMENTATION IS STRICTLI FOR X4)
+            device(str)         : Define the device to use
+            
 
         Returns:
-            _type_: _description_
+            np.ndarray: upscaled image
         """
         print('Upscaling...')
         upscaler_service.get_model(MODEL_PATH, tile, tile_pad, pre_pad, fp32, device)
--- RealESRGAN/train/scripts/train.py
+++ RealESRGAN/train/scripts/train.py
@@ -2,7 +2,7 @@
 ATTENTION!
 
 This file is not of my authorship; it is a copy from the excellent work created by user xinntao on GitHub.
-I have copied this work to make minor modifications necessary for my implementation in SageMaker.
+I've copied this file to make minor modifications necessary for my implementation in SageMaker.
 
 Original work by: xinntao (https://github.com/xinntao/Real-ESRGAN)
 """
