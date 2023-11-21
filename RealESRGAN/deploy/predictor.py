08b6ee7479dbe147f82ed86c5f7eeb4317ad2604
ndominutti <ndominutti@gmail.com>
2023-11-21 00:01:31 +0000

    Fix prediction process. CUDA forced in config file

--- RealESRGAN/deploy/predictor.py
+++ RealESRGAN/deploy/predictor.py
@@ -1,37 +1,30 @@
-from __future__ import print_function
-import json
-from fastapi import FastAPI, Response, UploadFile, Request, File
 from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi import FastAPI, Response, Request
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
+import utils
 import numpy as np
+import json
 import cv2
-import base64
 import io
-from PIL import Image
-
-model_path = "/opt/ml/model/net_g_latest.pth"
 
 
-kwargs = {
-            'tile':0,
-            'tile_pad':10,
-            'pre_pad':0,
-            'fp32':False,
-            'gpu_id':None,
-            'outscale':4
-        }
+config_dict = utils.load_config('/opt/program/config/config.yml')
+MODEL_PATH  = config_dict['model_path']
+KWARGS      = config_dict['model_params']
 
 
-class ScoringService(object):
+class UpscalingService(object):
+    """
+    """
     model = None
 
     @classmethod
-    def get_model(scoring_service, model_path, tile, tile_pad, pre_pad, fp32, gpu_id):
+    def get_model(upscaler_service, model_path, tile, tile_pad, pre_pad, fp32, device):
         """Get the model object for this instance, loading it if it's not already loaded."""
-        if scoring_service.model == None:
+        if upscaler_service.model == None:
             model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-            scoring_service.model = RealESRGANer(
+            upscaler_service.model = RealESRGANer(
                                     scale=4,
                                     model_path=model_path,
                                     dni_weight=None,
@@ -40,20 +33,20 @@ class ScoringService(object):
                                     tile_pad=tile_pad,
                                     pre_pad=int(pre_pad),
                                     half=not fp32,
-                                    gpu_id=gpu_id
+                                    gpu_id=None,
+                                    device=device
             )
             
     @classmethod
-    def preprocess(scoring_service, np_image):
+    def preprocess(upscaler_service, np_image):
         """
         """
         im2 = cv2.imdecode(np_image, cv2.IMREAD_UNCHANGED)
         return im2    
         
-        
     @classmethod
-    def predict(scoring_service, np_image, tile, tile_pad, pre_pad, fp32,
-                 gpu_id, outscale):
+    def predict(upscaler_service, np_image, tile, tile_pad, pre_pad, fp32,
+                 outscale, device):
         """For the input, do the predictions and return them.
 
         Args:
@@ -64,10 +57,10 @@ class ScoringService(object):
             _type_: _description_
         """
         print('Upscaling...')
-        scoring_service.get_model(model_path, tile, tile_pad, pre_pad, fp32, gpu_id)
-        img  = scoring_service.preprocess(np_image)
+        upscaler_service.get_model(MODEL_PATH, tile, tile_pad, pre_pad, fp32, device)
+        img  = upscaler_service.preprocess(np_image)
         try:
-            output, _ = scoring_service.model.enhance(img, outscale=outscale)
+            output, _ = upscaler_service.model.enhance(img, outscale=outscale)
         except RuntimeError as error:
             print('Error', error)
             print('If you encounter CUDA out of memory, try to set --tile with a smaller number.')
@@ -87,8 +80,9 @@ def read_root():
 def ping():
     """Determine if the container is working and healthy. In this sample container, we declare
     it healthy if we can load the model successfully."""
-    health = ScoringService.get_model() is not None
-    status  = 200 if health else 404
+    #Initialize the model
+    UpscalingService.get_model(MODEL_PATH, **{k:v for k,v in KWARGS.items() if k!='outscale'})
+    health =  UpscalingService.model is not None
     content = {"status":"Healthy container" if health else "Resource not found"}
     json_content = json.dumps(content)
     return JSONResponse(content=json_content)
@@ -108,6 +102,6 @@ async def predict(request: Request):
             status_code=415,
             media_type="text/plain",
         )
-    upscaled_img = ScoringService.predict(np_image, **kwargs)
+    upscaled_img = UpscalingService.predict(np_image, **KWARGS)
     _, img_encoded = cv2.imencode(".png", upscaled_img)
     return StreamingResponse(io.BytesIO(img_encoded), media_type="image/png")
