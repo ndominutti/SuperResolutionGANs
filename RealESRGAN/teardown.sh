16d24697b161bfac56afdee2046f4480a8e982b6
NicolasDominutti <ndominutti@gmail.com>
2023-11-28 19:54:51 -0300

    Add parameters to the teardown file

--- RealESRGAN/teardown.sh
+++ RealESRGAN/teardown.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+model_name=$1 
+endpoint_config_name=$2
 
 echo "Deleting S3 bucket..."
 aws s3 rm s3://realesrgan/train/ --recursive
@@ -16,7 +18,7 @@ aws ecr delete-repository --repository-name resrgan_training_job_image
 aws ecr delete-repository --repository-name  resrgan_inference_image
 
 echo "Deleting Sagemaker model..."
-aws sagemaker delete-model --model-name RealESRGAN-Model-19H-17M-52S 
+aws sagemaker delete-model --model-name ${model_name}
 echo "Deleting Sagemaker packages & packages group..."
 packages=$(aws sagemaker list-model-packages --model-package-group-name RealESRGAN --output text | awk '{print $4}')
 for package in $packages; do
@@ -24,6 +26,6 @@ for package in $packages; do
 done
 aws sagemaker delete-model-package-group --model-package-group-name RealESRGAN
 echo "Deleting Sagemaker endpoint configuration..."
-aws sagemaker delete-endpoint-config --endpoint-config-name RealESRGAN-Endpoint-Config-20H-31M-27S
+aws sagemaker delete-endpoint-config --endpoint-config-name ${endpoint_config_name}
 echo "Deleting Sagemaker endpoint..."
 aws sagemaker delete-endpoint --endpoint-name RealESRGAN-Endpoint
